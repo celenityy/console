4e626b52ae58556b33769ff5de251044c07b7be0
lberrymage <logan@lberrymage.dev>
2025-07-09 21:12:17 +0000

    Move routing away from NgModule
    This is the more modern approach in Angular and removes our last
    dependency on NgModules.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,16 +3,27 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { provideRouter } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { GlobalErrorHandler } from './global-error-handler';
+import { routes } from './app.routes';
 import { unauthorizedInterceptor } from './unauthorized.interceptor';
 import { withCredentialsInterceptor } from './with-credentials.interceptor';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        importProvidersFrom(AppRoutingModule),
+        {
+            provide: ErrorHandler,
+            useClass: GlobalErrorHandler,
+        },
         provideHttpClient(withInterceptors([unauthorizedInterceptor, withCredentialsInterceptor])),
+        {
+            provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+            useValue: { duration: 5000 },
+        },
+        provideRouter(routes),
         provideZoneChangeDetection({ eventCoalescing: true }),
     ],
 };
