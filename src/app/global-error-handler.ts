6c899347feb19f336205a1ee439ed493b58bafed
lberrymage <logan@lberrymage.dev>
2025-07-09 21:23:08 +0000

    Remove unnecessary use of `NgZone`

--- src/app/global-error-handler.ts
+++ src/app/global-error-handler.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import { ErrorHandler, Injectable, NgZone, inject } from '@angular/core';
+import { ErrorHandler, Injectable, inject } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -13,12 +13,11 @@ import { isApiError } from './api-error';
 })
 export class GlobalErrorHandler implements ErrorHandler {
     private snackbar = inject(MatSnackBar);
-    private zone = inject(NgZone);
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     handleError(error: any): void {
         if (error instanceof HttpErrorResponse && isApiError(error.error)) {
-            this.zone.run(() => this.snackbar.open(error.error.message));
+            this.snackbar.open(error.error.message);
         } else {
             console.error(error);
         }
